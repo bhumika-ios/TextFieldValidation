e8f8e58d62838d01a4a11311f7526183c50e0d6b
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-13 15:48:04 +0530

    email validation call save button used textfield email

--- TextFieldValidation/ViewController.swift
+++ TextFieldValidation/ViewController.swift
@@ -17,18 +17,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnSave(_ sender: Any) {
+        if (txtEmail.text?.isValidEmail)!{
+            print("your email is valid")
+            
+        }else{
+            print("your email is not valid")
+        }
     }
     
 }
 
 extension String{
-    var isMobileNumber: Bool{
+    var isPhoneNumber: Bool{
         do{
             let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
             let matches = detector.matches(in: self,options: [], range: NSMakeRange(0, self.count))
             if let res = matches.first {
                 return res.resultType == .phoneNumber && res.range.location == 0 && res.range.length == self.count && self.count == 10
-            } else {
+                } else {
+                    return false
+                }
+            }  catch {
                 return false
             }
         }
@@ -36,10 +45,13 @@ extension String{
             let passwordTest = NSPredicate(format: "self matched %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
             return passwordTest.evaluate(with: self)
         }
+        // email validation
         var isValidEmail: Bool{
+            //{3} means .com for used
             let emailRegxEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{3}"
+            //predicate means find array- same as filter
             let emailTest = NSPredicate(format: "self matches %@", emailRegxEx)
             return emailTest.evaluate(with: self)
         }
-    }
+    
 }
