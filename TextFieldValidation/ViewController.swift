bb9a980c383ae1338f5feb125c94a6d535f33a5f
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-13 15:32:18 +0530

    validation code create mobile number and password - create extension of string in viewcontroller

--- TextFieldValidation/ViewController.swift
+++ TextFieldValidation/ViewController.swift
@@ -9,14 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var txtEmail: UITextField!
-    
-    @IBOutlet var txtMobileNo: UIButton!
     @IBOutlet var txtPass: UITextField!
+    @IBOutlet var txtMobileNo: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSave(_ sender: Any) {
+    }
+    
 }
 
+extension String{
+    var isMobileNumber: Bool{
+        do{
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+            let matches = detector.matches(in: self,options: [], range: NSMakeRange(0, self.count))
+            if let res = matches.first {
+                return res.resultType == .phoneNumber && res.range.location == 0 && res.range.length == self.count && self.count == 10
+            } else {
+                return false
+            }
+        }
+        var isPasswordValid: Bool {
+            let passwordTest = NSPredicate(format: "self matched %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+            return passwordTest.evaluate(with: self)
+        }
+        
+    }
+}
